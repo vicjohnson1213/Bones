c9c58da0d7efc6427b9b55aeaf5586244ab88ead
Victor Johnson <vicjohnson1213@gmail.com>
2019-01-20 23:13:32 -0600

    Included utils in the package.

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     description='A bare-bones command-line options parser',
     author='Victor Johnson',
     author_email='vicjohnson@gmail.com',
-    py_modules=['bones']
+    py_modules=['bones', 'utils']
 )
